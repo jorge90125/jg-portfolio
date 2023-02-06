2c36d71f92442cf47c47038fa858e1c47ec54fcb
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2023-02-05 21:26:38 -0600

    React app working with Tailwind

--- jg-portfolio/src/App.js
+++ jg-portfolio/src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Nav from './components/Nav'
+import AboutMe from './components/AboutMe'
+import Resume from './components/Resume'
+import Contact from './components/Contact'
+import BeatTheBullyProject from './components/BeatTheBullyProject'
+import MyCartProject from './components/MyCartProject'
+import PortfolioProject from './components/PortfolioProject'
+import StaxOnStaxProject from './components/StaxOnStaxProject'
+import StreaminProject from './components/StreaminProject'
 
-function App() {
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+library.add(faSquareGithub)
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-  );
+    <div>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<AboutMe />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/beatthebully' element={<BeatTheBullyProject />} />
+        <Route path='/mycart' element={<MyCartProject />} />
+        <Route path='/portfolio' element={<PortfolioProject />} />
+        <Route path='/staxonstax' element={<StaxOnStaxProject />} />
+        <Route path='/streamin' element={<StreaminProject />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
