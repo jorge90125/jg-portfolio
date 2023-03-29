de908a83819a6a7e6ffdced1b12f5ed4899c9923
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2023-03-29 13:26:53 -0500

    Project pages almost all up to date

--- jg-portfolio/src/components/StaxOnStaxProject.js
+++ jg-portfolio/src/components/StaxOnStaxProject.js
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const StaxOnStaxProject = () => {
     return(
-        <div class="text-blue2 text-lg tracking-wide px-10 leading-relaxed">
+        <div class="text-blue2 text-lg tracking-wide leading-relaxed">
             <h1 class="text-blue5 text-4xl text-center font-mono uppercase tracking-widest py-8">
                 Stax On Stax
             </h1>
-            <div class="ml-36">
-                <img class="float-right pt-6 ml-6 mb-8" src='stax-on-stax-ss.png' width='600'/>
-                <p class="indent-10 pt-6 mb-6 text-right">
+            <div class="ml-16 mb-10">
+                <img class="float-right pt-6 mx-8 mb-8" src='stax-on-stax-ss.png' width='600'/>
+                <p class="pt-6 mb-6 text-right">
                     Stax on Stax is the capstone project for my software engineering immersive course at General assembly. Stax on Stax is a full-stack React based app that allows users to keep track of their vinyl record collections. Users can regster, sign-in, sign-out, and the app utilizes user authentication to perform CRUD functions. Users can create, edit, delete, and not only view their collections, but the collections of any other user. The app also allows the user to filter their collections, ascending or descending, based on several fields.
                 </p>
                 <div class="flex flex-row justify-evenly">
@@ -27,34 +27,34 @@ const StaxOnStaxProject = () => {
                     </div>
                 </div>
             </div>
-                <h3 class="text-2xl mt-8 font-mono uppercase tracking-widest text-center text-blue5">
-                    Utilized Skills and Languages
-                </h3>
-                <div class="flex justify-left justify-evenly">
-                    <div>
-                        <h4 class="text-xl underline text-blue3">
-                            Backend
-                        </h4>
-                        <ul class="list-disc list-inside leading-5">
-                            <li>Python</li>
-                            <li>Flask</li>
-                            <li>Sql</li>
-                            <li>Dotenv</li>
-                            <li>Bcrypt</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h4 class="text-xl underline text-blue3">
-                            Frontend
-                        </h4>
-                        <ul class="list-disc list-inside leading-5">
-                            <li>React</li>
-                            <li>Javascript</li>
-                            <li>CSS</li>
-                            <li>Node.js</li>
-                        </ul>
-                    </div>
+            <h3 class="text-2xl mt-8 font-mono uppercase tracking-widest text-center text-blue5">
+                Utilized Skills and Languages
+            </h3>
+            <div class="flex justify-left justify-evenly pb-10">
+                <div>
+                    <h4 class="text-xl underline text-blue3">
+                        Backend
+                    </h4>
+                    <ul class="list-disc list-inside leading-5">
+                        <li>Python</li>
+                        <li>Flask</li>
+                        <li>Sql</li>
+                        <li>Dotenv</li>
+                        <li>Bcrypt</li>
+                    </ul>
                 </div>
+                <div>
+                    <h4 class="text-xl underline text-blue3">
+                        Frontend
+                    </h4>
+                    <ul class="list-disc list-inside leading-5">
+                        <li>React</li>
+                        <li>Javascript</li>
+                        <li>CSS</li>
+                        <li>Node.js</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- jg-portfolio/src/components/StreaminProject.js
+++ jg-portfolio/src/components/StreaminProject.js
@@ -3,51 +3,61 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const StreaminProject = () => {
     return(
-        <div>
-            <h1>
+        <div class="text-blue2 text-lg tracking-wide leading-relaxed">
+            <h1 class="text-blue5 text-4xl text-center font-mono uppercase tracking-widest py-8">
                 Streamin
             </h1>
-            <div>
-                <h3>Frontend -  </h3><h1><a href='https://github.com/Bernard-Calma/streamin-frontend' target="_blank"><FontAwesomeIcon icon="fa-brands fa-square-github" /></a></h1>
+            <div class="ml-16 mb-10">
+                <img class="float-right pt-6 mx-8 mb-8" src='streamin-ss.png' width='600'/>
+                <p class="pt-6 mb-6 text-right">
+                    Streamin is the group project for for my software engineering immersive course at General assembly. It was a full-stack MERN app collaboration between myself, Bernard Calma, and Rousse Bidon. Streamin uses user authentication to allow users to post embeded videos from most popular streaming platform with full CRUD capabillities. Users can also like videos that other users have posted. As well as comment on their own and eachothers' videos and like said comments. The app also allows the user to search the database of all videos based on the titles of the uploaded videos. My main responsibilities were to work on user verification, the register route, the comment function, the like function, as well as some miscellaneous frontend work.
+                </p>
+                <div class="flex flex-row justify-evenly">
+                    <div class="flex text-6xl h-20 flex-col items-center hover:text-blue3">
+                        <a class="hover:animate-bounce" href='https://github.com/Bernard-Calma/streamin-frontend' target="_blank">
+                            <FontAwesomeIcon icon="fa-brands fa-square-github" />
+                        </a>
+                        <h3 class="text-base">
+                            Frontend
+                        </h3>
+                    </div>
+                    <div class="flex text-6xl h-20 flex-col items-center hover:text-blue3">
+                        <a class="hover:animate-bounce" href='https://github.com/Bernard-Calma/streamin-backend' target="_blank">
+                            <FontAwesomeIcon icon="fa-brands fa-square-github" />
+                        </a>
+                        <h3 class="text-base">
+                            Backend
+                        </h3>
+                    </div>
+                </div>
             </div>
-            <div>
-                <h3>Backend -  </h3><h1><a href='https://github.com/Bernard-Calma/streamin-backend' target="_blank"><FontAwesomeIcon icon="fa-brands fa-square-github" /></a></h1>
-            </div>
-            <p>
-                Streamin is the group project for for my software engineering immersive course at General assembly.
-            <br/>
-                It was a full-stack MERN app collaboration between myself, Bernard Calma, and Rousse Bidon.
-            <br/>
-                Streamin uses user authentication to allow users to post embeded videos from most popular streaming platform with full CRUD capabillities.
-            <br/>
-                Users can also like videos that other users have posted. As well as comment on their own and eachothers' videos and like said comments.
-            <br/>
-                The app also allows the user to search the database of all videos based on the titles of the uploaded videos.
-            <br/>
-                My main responsibilities were to work on user verification, the register route, the comment function, the like function, as well as some miscellaneous frontend work.
-            </p>
-            <h3>
+            <h3 class="text-2xl mt-8 font-mono uppercase tracking-widest text-center text-blue5">
                 Utilized Skills and Languages
             </h3>
-            <h4>
-                Backend
-            </h4>
-            <ul class="list-disc list-inside">
-                <li>MongoDB</li>
-                <li>Express.js</li>
-                <li>Node.js</li>
-            </ul>
-            <h4>
-                Frontend
-            </h4>
-            <ul class="list-disc list-inside">
-                <li>React</li>
-                <li>JSX</li>
-                <li>Javascript</li>
-                <li>HTML</li>
-                <li>CSS</li>
-            </ul>
-            <img src='streamin-ss.png' width='600'/>
+            <div class="flex justify-left justify-evenly pb-10">
+                <div>
+                    <h4 class="text-xl underline text-blue3">
+                        Backend
+                    </h4>
+                    <ul class="list-disc list-inside leading-5">
+                        <li>MongoDB</li>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="text-xl underline text-blue3">
+                        Frontend
+                    </h4>
+                    <ul class="list-disc list-inside leading-5">
+                        <li>React</li>
+                        <li>JSX</li>
+                        <li>Javascript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
